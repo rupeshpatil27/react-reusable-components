7e49f45db53611d5d7dc8c6d56bc5d169f5bffbb
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-11 23:48:21 +0530

    setup timeline project

---
+++ timeline/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="sm:px-10 px-5 lg:px-15 min-h-screen pt-20 md:pt-30 z-10">
+      <h2 className="font-bold text-3xl md:text-4xl text-center">Timeline</h2>
+    </div>
+  );
+};
+
+export default App;
